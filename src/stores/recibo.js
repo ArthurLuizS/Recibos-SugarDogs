de8162a9e73efe8059178bdedbca3229b9091218
Arthur Luiz <arthurluiz36@gmail.com>
2024-07-21 20:20:28 -0300

    adicionada logica de desconto por serviço ao store

--- src/stores/recibo.js
+++ src/stores/recibo.js
@@ -13,21 +13,31 @@ export const useRecibo = defineStore("recibo", {
       serviceData: [
         {
           type: null,
-          quantity: null,
+          quantity: 1,
           unitPrice: null,
           discount: null,
+          servicePrice: null,
         },
       ],
     },
   }),
   getters: {
     doubleCount: (state) => state.counter * 2,
+    discount: (state) => {
+      for (let i = 0; i < state.form.serviceData.length; i += 1) {
+        let service = state.form.serviceData[i];
+        service.servicePrice =
+          parseInt(service.unitPrice) * parseInt(service.quantity) -
+          (parseInt(service.discount) / 100) *
+            (parseInt(service.unitPrice) * parseInt(service.quantity));
+      }
+    },
   },
   actions: {
     moreService() {
       this.form.serviceData.push({
         type: null,
-        quantity: null,
+        quantity: 1,
         unitPrice: null,
         discount: null,
       });
